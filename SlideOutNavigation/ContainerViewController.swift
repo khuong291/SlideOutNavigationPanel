afb102271bda643d758130083f966ff559ba601e
Khuong Pham <dkhuong291@gmail.com>
2016-02-29 14:04:31 +0700

    Init

---
+++ SlideOutNavigation/ContainerViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ContainerViewController.swift
+//  SlideOutNavigation
+//
+//  Created by James Frost on 03/08/2014.
+//  Copyright (c) 2014 James Frost. All rights reserved.
+//
+
+import UIKit
+import QuartzCore
+
+class ContainerViewController: UIViewController {
+
+    var centerNavigationController: UINavigationController!
+    var centerViewController: CenterViewController!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        centerViewController = UIStoryboard.centerViewController()
+        centerViewController.delegate = self
+
+        centerNavigationController = UINavigationController(rootViewController: centerViewController)
+        view.addSubview(centerNavigationController.view)
+        addChildViewController(centerNavigationController)
+    }
+
+}
+
+private extension UIStoryboard {
+    class func mainStoryboard() -> UIStoryboard { return UIStoryboard(name: "Main", bundle: NSBundle.mainBundle()) }
+
+    class func leftViewController() -> SidePanelViewController? {
+        return mainStoryboard().instantiateViewControllerWithIdentifier("LeftViewController") as? SidePanelViewController
+    }
+
+    class func rightViewController() -> SidePanelViewController? {
+        return mainStoryboard().instantiateViewControllerWithIdentifier("RightViewController") as? SidePanelViewController
+    }
+
+    class func centerViewController() -> CenterViewController? {
+        return mainStoryboard().instantiateViewControllerWithIdentifier("CenterViewController") as? CenterViewController
+    }
+    
+}
